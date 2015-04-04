e340e8168d6cfea0fda2ee8c76427323194fed69
Nikhil John <nikhiljohn1010@gmail.com>
2015-04-04 22:35:00 +0530

    0.0.2 - Layout half finished Fix 1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,13 @@
 
 $(document).ready(function(){
 	$('#masthead span.toggle').click(function(){
-		$(this).toggleClass('open');
+		$(this).addClass('open');
 		$('#nav-main-menu').toggle( "fade" );
+	});
+	$('main').click(function(){
+		if($('#masthead span.toggle').hasClass('open')){
+			$('#masthead span.toggle').removeClass('open');
+			$('#nav-main-menu').toggle( "fade" );
+		}	
 	});
 });
