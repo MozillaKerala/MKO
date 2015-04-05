ea9316bd6fc21b11034e37cf24a74357810dc5f5
Nikhil John <nikhiljohn1010@gmail.com>
2015-04-05 13:53:09 +0530

    0.1.0 - Layout complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,4 +14,15 @@ $(document).ready(function(){
 	$('button.durl').click(function() {
 		window.open($(this).attr('data-redirect-url'), '_blank');
 	});
+	$( "div.next-event-nav" ).click(function() {
+		$( "div.more-events" ).animate({
+				height: "toggle"
+			}, 1500,function(){
+				if($("div.next-event-nav .glyphicon").hasClass("glyphicon-menu-down")) {
+					$('div.next-event-nav>span').switchClass( "glyphicon-menu-down", "glyphicon-menu-up", 1000 );
+				} else {
+					$('div.next-event-nav>span').switchClass( "glyphicon-menu-up", "glyphicon-menu-down", 1000 );
+				}
+		});
+	});
 });
